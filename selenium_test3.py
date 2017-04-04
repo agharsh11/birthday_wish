cc7908ba052fa9190ed752a9f0226563d88ea832
harsh <agharsh11@gmail.com>
2017-04-04 17:12:59 +0530

    added mouse hover

--- selenium_test3.py
+++ selenium_test3.py
@@ -2,15 +2,16 @@ import time
 from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup as bs
+from selenium.webdriver import ActionChains
 driver = webdriver.Chrome('/home/harsh/Desktop/chromedriver')  # Optional argument, if not specified will search path.
 driver.get('https://www.facebook.com/');
 time.sleep(1) 
 search_box = driver.find_element_by_name('email')
-search_box.send_keys('daal_lena@gmail.com')
+search_box.send_keys('@gmail.com')
 #search_box.submit()
 #time.sleep(5)
 search_box = driver.find_element_by_name('pass')
-search_box.send_keys('daal lena')
+search_box.send_keys('')
 search_box.submit()
 #src=requests.get("").text
 time.sleep(5)
@@ -58,7 +59,7 @@ for i in soup2:
 	soup3=i.find_all('div',class_="_50f8 _50f4")[0]
 	title="".join([str(j) for j in soup3.contents])
 	print title
-	if title == "Birthday is in 3 days" :
+	if title == "Birthday is in 2 days" :
 		k=i["href"]
 		break
 print k
@@ -68,18 +69,26 @@ driver.get(link)
 print link
 time.sleep(15) 
 try:
-	print dir(search_box)
-	search_box=driver.find_elements_by_xpath('//*[@id="js_1"]/form')
-	print "rolling today"
+	#print dir(search_box)
+	print "going"
+	search_box=driver.find_elements_by_xpath('//*[@id="rc.u_0_1g"]/div[1]/div/div[1]/div/a[1]')
+	#el = driver.find_element_by_id('js_gh')
+	print "found element"
+	hover = ActionChains(driver).move_to_element(search_box)
+	print "defined hover"
+	hover.click().build().perform()
+	print "defined hover"
+
+	#print dir(search_box)
 #[@id="js_23"]/div[1]/div/div[1]/div[2]/div/div/div/div/div[2]/div/div/div/div
 except:
 	pass
 
-print dir(search_box)
+print "out"
 #search_box.click()
 #search_box.send_keys('testing')
 #search_box.submit()
 
 #use the #collection_wrapper_2356318349 to uniquely pinpoint birthdays
-time.sleep(100) # Let the user actually see something!
+time.sleep(30) # Let the user actually see something!
 driver.quit()
